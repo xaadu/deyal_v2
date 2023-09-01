01530a39faa677e1b7b40a3c2aa650224f83894f
xaadu <abdullahzayed01@gmail.com>
2023-09-02 02:29:12 +0600

    [ ADD ] pagination on blogs

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -4,12 +4,29 @@ import blogImg from '../../assets/blog.png'
 
 const Blogs = () => {
 
+    const [pageNum, setPageNum] = useState(1);
+
+    const [nextPage, setNextPage] = useState(null);
+    const [prevPage, setPrevPage] = useState(null);
+
     const [blogs, setBlogs] = useState([]);
     useEffect(() => {
-        fetch('/blogs.json')
+        fetch(`http://127.0.0.1:8000/blogs/?page=${pageNum}&page_size=1`)
             .then(res => res.json())
-            .then(data => setBlogs(data))
-    }, []);
+            .then(data => {
+                setBlogs(data?.results);
+                setNextPage(data?.next);
+                setPrevPage(data?.previous);
+            });
+    }, [pageNum]);
+
+    const handleNextPage = (e) => {
+        if (nextPage) setPageNum(old_value => old_value + 1)
+    }
+    const handlePrevPage = (e) => {
+        if (prevPage) setPageNum(old_value => old_value - 1)
+    }
+
     return (
         <div>
             <section className="container">
@@ -25,22 +42,37 @@ const Blogs = () => {
                 </div>
             </section>
             <section className="container">
-                <h1 className="my-5 fw-bolder text-main">Our Recent Blogs</h1>
+                <h1 className="my-5 fw-bolder text-main">Our Blogs</h1>
                 <div className="row g-4">
                     {
                         blogs.map((blog) => (
                             <div className="col-12" key={blog?.id}>
                                 <div className="p-5 mb-2 shadow box rounded-20 text-start">
                                     <h3>{blog?.title}</h3>
-                                    <span className="px-2 py-1 btn-main pe-auto rounded-3"><i className="far fa-calendar-alt"></i> {blog?.date}</span>
+                                    <span className="px-2 py-1 btn-main pe-auto rounded-3">
+                                        <i className="far fa-calendar-alt"></i> {blog?.date}
+                                    </span>
                                     <br />
                                     <small className="mt-4 d-block text-second">{blog?.des}</small>
-                                    <Link className="mt-3 btn btn-outline-main" to="/blogpost">Read More</Link>
+                                    {/* <Link className="mt-3 btn btn-outline-main" to="/blogpost">Read More</Link> */}
                                 </div>
                             </div>
                         ))
                     }
                 </div>
+                <button
+                    className={`my-3 btn btn-outline-main ${!prevPage && "disabled"}`}
+                    onClick={handlePrevPage}
+                >
+                    Previous
+                </button>
+                <button className="my-3 mx-2 btn btn-outline-main disabled" disabled>{pageNum}</button>
+                <button
+                    className={`my-3 btn btn-outline-main ${!nextPage && "disabled"}`}
+                    onClick={handleNextPage}
+                >
+                    Next
+                </button>
             </section>
         </div>
     );

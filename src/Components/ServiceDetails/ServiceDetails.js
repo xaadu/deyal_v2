48ef71876a8bb19dcfea257ab2806b8750f85f40
xaadu <abdullahzayed01@gmail.com>
2023-09-02 00:55:36 +0600

    [ ADD ] dynamic form for appointment

--- src/Components/ServiceDetails/ServiceDetails.js
+++ src/Components/ServiceDetails/ServiceDetails.js
@@ -1,11 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useState } from 'react';
 import { useParams } from 'react-router';
 import dpic from '../../assets/datepick.png';
+import { getAuth } from 'firebase/auth';
 
 const ServiceDetails = () => {
+    const auth = getAuth();
     const { serviceId } = useParams();
     // const key = parseInt(serviceId);
+
+    const [weekDates, setWeekDates] = useState([]);
+    const [formMsz, setFormMsz] = useState("");
+
+    const doctorInput = useRef(null);
+    const appointmentTimeInput = useRef(null);
 
     const [serviceDetails, setServiceDetails] = useState({});
     useEffect(() => {
@@ -18,9 +26,52 @@ const ServiceDetails = () => {
     useEffect(() => {
         fetch(`http://127.0.0.1:8000/therapists/?speciality_id=${serviceId}`)
             .then(res => res.json())
-            .then(data => setTherapists(data))
+            .then(data => setTherapists(data));
     }, [serviceId]);
     // const person = therapists.find(({ id }) => id === key);
+
+    const handleAppointment = (e) => {
+        e.preventDefault();
+        const data = {
+            "appointment_time": e.target.appointmentTime.value,
+            "full_name": e.target.fullName.value,
+            "email": e.target.email.value,
+            "current_age": e.target.currentAge.value,
+            "phone_no": e.target.phoneNo.value,
+            "gender": e.target.gender.value,
+            "site_user": auth.currentUser.email,
+        }
+        fetch(
+            `http://127.0.0.1:8000/therapists/appointment-bookings/`,
+            {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json;',
+                    'Content-Type': 'application/json;',
+                },
+                body: JSON.stringify(data),
+            }
+        )
+            .then(res => res.json())
+            .then(data => {
+                if (data?.id) {
+                    setFormMsz("We'll let you know your date and time through email. Thank you.");
+                    e.target.remove();
+                } else { }
+            });
+    }
+    const UpdateAppointmentTime = (e) => {
+        setWeekDates([]);
+        const therapist_id = doctorInput.current.value;
+        const appointment_type = appointmentTimeInput.current.value;
+        const qs = `therapist_id=${therapist_id}&appointment_type=${appointment_type}`
+        if (therapist_id && appointment_type)
+            fetch(
+                `http://127.0.0.1:8000/therapists/appointment-times/?${qs}`
+            )
+                .then(res => res.json())
+                .then(data => setWeekDates(data));
+    }
 
     return (
         <div className="container text-start">
@@ -60,14 +111,16 @@ const ServiceDetails = () => {
                     </ul>
                 </div>
                 <div>
-                    <h3 className="fw-bolder">How to cure <span className="text-main">{serviceDetails?.title}</span> ?</h3>
+                    <h3 className="fw-bolder">
+                        How to cure <span className="text-main">{serviceDetails?.title}</span> ?
+                    </h3>
                     <p className="py-3">{serviceDetails?.cure}</p>
                 </div>
             </section>
             <section className="container">
                 <h1 className="mt-5 text-center fw-bolder">Recomended <span className="text-main">Therapists</span></h1>
                 {
-                    therapists && therapists.map((therapist) => {
+                    therapists.map((therapist) => {
                         return (
                             <div className="row justify-content-center">
                                 <div className="py-5 col-lg-8">
@@ -98,51 +151,95 @@ const ServiceDetails = () => {
                         <img className="img-fluid" src={dpic} alt="date pick" />
                     </div>
                     <div className="col-lg-6">
-                        <div className="form-body">
-                            <div className="my-2 col-md-12">
-                                <input className="form-control" type="text" name="name" placeholder="Your Full Name" required />
-                            </div>
-                            <div className="my-2 col-md-12">
-                                <input className="form-control" type="email" name="email" placeholder="E-mail Address" required />
-                            </div>
-                            <div className="my-2 col-md-12">
-                                <input className="form-control" type="number" name="number" placeholder="Your Current Age" required />
-                            </div>
-                            <div className="my-2 col-md-12">
-                                <select className="mt-3 form-select" required>
-                                    <option disabled defaultValue="">Pick A Week Date</option>
-                                    <option defaultValue="jweb">Satday</option>
-                                    <option defaultValue="sweb">Monday</option>
-                                    <option defaultValue="pmanager">Wednesday</option>
-                                </select>
-                            </div>
+                        <p className='fw-bold text-success'>{formMsz}</p>
+                        <form onSubmit={handleAppointment}>
+                            <div className="form-body">
+                                <div className="my-2 col-md-12">
+                                    <select
+                                        className="mt-3 form-select"
+                                        required
+                                        onChange={UpdateAppointmentTime}
+                                        ref={doctorInput}
+                                        name="therapist_id"
+                                    >
+                                        <option disabled selected value="">Choose Doctor</option>
+                                        {
+                                            therapists.map((therapist) => {
+                                                return <option value={therapist?.id}>{therapist?.name}</option>
+                                            })
+                                        }
+                                    </select>
+                                </div>
+                                <div className="my-2 col-md-12">
+                                    <select
+                                        className="form-select"
+                                        required
+                                        onChange={UpdateAppointmentTime}
+                                        ref={appointmentTimeInput}
+                                        name="appointment_type"
+                                    >
+                                        <option disabled selected value="">Choose Appointment Type</option>
+                                        <option value="online">Online</option>
+                                        <option value="offline">Offline</option>
+                                    </select>
+                                </div>
+                                <div className="my-2 col-md-12">
+                                    <select className="form-select" name="appointmentTime" required>
+                                        <option disabled selected value="">Pick A Week Date</option>
+                                        {
+                                            weekDates.map((weekDate) => {
+                                                return <option value={weekDate?.id}>{weekDate?.details}</option>
+                                            })
+                                        }
+                                    </select>
+                                </div>
+                                <div className="my-2 col-md-12">
+                                    <input className="form-control mt-3" type="text" name="fullName" placeholder="Your Full Name" required />
+                                </div>
+                                <div className="my-2 col-md-12">
+                                    <input className="form-control" type="email" name="email" placeholder="E-mail Address" required />
+                                </div>
+                                <div className="my-2 col-md-12">
+                                    <input className="form-control" type="number" name="currentAge" placeholder="Your Current Age" required />
+                                </div>
+                                <div className="my-2 col-md-12">
+                                    <input className="form-control" type="text" name="phoneNo" placeholder="Your Phone Number (Optional)" required />
+                                </div>
+                                <div className="my-2 col-md-12">
+                                    <label className="mb-3 me-2 fw-bolder text-second" htmlFor="gender">Gender: </label>
 
-                            <div className="my-2 col-md-12">
-                                <label className="p-1 mb-3 rounded-3 me-3 btn-main" htmlFor="pickTime">
-                                    Pick Your Suitable Time
-                                </label>
-                                <input type="time" name="" id="pickTime" />
+                                    <input
+                                        type="radio" className="btn-check"
+                                        name="gender" id="male" autoComplete="off"
+                                        value="male" required
+                                    />
+                                    <label className="btn btn-sm btn-outline-main me-3" htmlFor="male">Male</label>
+
+                                    <input
+                                        type="radio" className="btn-check"
+                                        name="gender" id="female" autoComplete="off"
+                                        value="female" required
+                                    />
+                                    <label className="btn btn-sm btn-outline-main me-3" htmlFor="female">Female</label>
+
+                                    <input
+                                        type="radio" className="btn-check"
+                                        name="gender" id="other" autoComplete="off"
+                                        value="other" required
+                                    />
+                                    <label className="btn btn-sm btn-outline-main" htmlFor="other">Other</label>
+                                </div>
+
+                                {/* <div className="form-check">
+                                    <input className="form-check-input text-main" type="checkbox" defaultValue="" id="invalidCheck" required />
+                                    <label className="form-check-label">I confirm that all data are correct</label>
+                                </div> */}
+
+                                <div className="mt-3 form-button">
+                                    <button id="submit" type="submit" className="btn btn-main">Register</button>
+                                </div>
                             </div>
-
-                            <div className="my-2 col-md-12">
-                                <label className="mb-3 me-2 fw-bolder text-second" htmlFor="gender">Gender: </label>
-
-                                <input type="radio" className="btn-check" name="gender" id="male" autoComplete="off" required />
-                                <label className="btn btn-sm btn-outline-main me-3" htmlFor="male">Male</label>
-
-                                <input type="radio" className="btn-check" name="gender" id="female" autoComplete="off" required />
-                                <label className="btn btn-sm btn-outline-main" htmlFor="female">Female</label>
-                            </div>
-
-                            <div className="form-check">
-                                <input className="form-check-input text-main" type="checkbox" defaultValue="" id="invalidCheck" required />
-                                <label className="form-check-label">I confirm that all data are correct</label>
-                            </div>
-
-                            <div className="mt-3 form-button">
-                                <button id="submit" type="submit" className="btn btn-main">Register</button>
-                            </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </section>
